ba7b778055f7ab60c6817680dfd5e427a1421cdf
rikiulirniam <rikiulir@gmail.com>
2026-06-21 13:35:46 +0700

    fix: docker adjustment

--- api/server.js
+++ api/server.js
@@ -10,18 +10,24 @@ const APP_PORT = process.env.APP_PORT ?? 3000;
 connectDB();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200
+  origin: ["http://localhost:5173", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRouter);
+
+app.use(express.static(path.join(__dirname, "public-react")));
 app.all("*any", (req, res) => {
-  res.status(404).json({ message: "Endpoint tidak ditemukan" });
+  try {
+    res.sendFile(path.join(__dirname, "public-react", "index.html"));
+  } catch (error) {
+    res.status(404).json({ message: "Endpoint tidak ditemukan" });
+  }
 });
 app.listen(APP_PORT, () => {
   console.log(`Server is running on port ${APP_HOST}:${APP_PORT}`);
